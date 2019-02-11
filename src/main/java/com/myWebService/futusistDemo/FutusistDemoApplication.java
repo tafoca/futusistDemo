786a98f768b11eefb594539c1aad4571743b16b7
tafoca <konohamarufotso@gmail.com>
2019-02-11 14:24:04 +0100

    initial commit and loading all basics dependencies

---
+++ src/main/java/com/myWebService/futusistDemo/FutusistDemoApplication.java
@@ -0,0 +1,14 @@
+package com.myWebService.futusistDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutusistDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutusistDemoApplication.class, args);
+	}
+
+}
+
